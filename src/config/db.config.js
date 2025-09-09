83a7e7590bae2f45ab9400eef5f929c8f5b8caad
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-09-10 00:41:14 +0530

    made changes in sequelize instance for render deployment

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,17 +2,23 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv"
 dotenv.config()
 
+// For local development
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host : process.env.DB_HOST,
+//         dialect : "mysql",
+//         logging : false,
+//     }
+// )
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host : process.env.DB_HOST,
-        dialect : "mysql",
-        logging : false,
-    }
-)
+// For render deployment
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect : "postgres",
+    logging : false
+})
 
 
 
