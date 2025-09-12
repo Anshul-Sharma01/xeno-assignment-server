6a73d3f3e9b2e8e37b7ff1b8481186d6517bae43
Anshul Sharma <sharma.ansh2926@gmail.com>
2025-09-12 18:11:50 +0530

    refactored db config for render deployment

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,22 +3,22 @@ import dotenv from "dotenv"
 dotenv.config()
 
 // For local development
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host : process.env.DB_HOST,
-        dialect : "mysql",
-        logging : false,
-    }
-)
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//         host : process.env.DB_HOST,
+//         dialect : "mysql",
+//         logging : false,
+//     }
+// )
 
 // For render deployment
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//     dialect : "postgres",
-//     logging : false
-// })
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect : "postgres",
+    logging : false
+})
 
 
 
